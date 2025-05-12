d40d71477a043791980e4753dfbbe8075f9b0ca2
서제효 <jaehyo0318@kakao.com>
2025-05-12 11:46:12 +0900

    sourceTree Test push

--- ReGo/Sources/Models/Model.swift
+++ ReGo/Sources/Models/Model.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
+import SwiftData
+
+enum RestrospectCategory: String, Codable {
+    case a = "카테고리a"
+    case b = "카테고리b"
+    case c = "카테고리c"
+}
+
+@Model
+class Restrospect: Identifiable {
+    var id: UUID = UUID()
+    var date: Date
+    var category: RestrospectCategory
+    var title: String
+    var content: String
+
+    init(date: Date, category: RestrospectCategory, title: String, content: String) {
+        self.date = date
+        self.category = category
+        self.title = title
+        self.content = content
+    }
+}
