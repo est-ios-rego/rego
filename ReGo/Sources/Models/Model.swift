266703fb65e7712d720e9d0e38b0b92d35abe40b
서제효 <jaehyo0318@kakao.com>
2025-05-12 15:27:35 +0900

    Revert "Add Retrospect Model File"
    This reverts commit 9a26332cf1abfb4e27c31e270381dce3f859c6c6.
    
    # Conflicts:
    #	ReGo/Sources/Models/Model.swift

--- ReGo/Sources/Models/Model.swift
+++ ReGo/Sources/Models/Model.swift
@@ -6,33 +6,3 @@
 //
 
 import Foundation
-import SwiftUI
-import SwiftData
-
-import Foundation
-import SwiftUI
-import SwiftData
-
-enum RetrospectCategory: String, Codable {
-    case category1 = "category1"
-    case category2 = "category2"
-    case category3 = "category3"
-    case category4 = "category4"
-    case category5 = "category5"
-}
-
-@Model
-class Retrospect: Identifiable {
-    var id: UUID = UUID()
-    var title: String
-    var content: String
-    var date: Date
-    var category: RetrospectCategory
-
-    init(title: String, content: String, date: Date, category: RetrospectCategory) {
-        self.title = title
-        self.content = content
-        self.date = date
-        self.category = category
-    }
-}
