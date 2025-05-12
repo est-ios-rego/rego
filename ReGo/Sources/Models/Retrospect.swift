9a26332cf1abfb4e27c31e270381dce3f859c6c6
성주현 <qpqpqoqo12@gmail.com>
2025-05-12 14:28:47 +0900

    Add Retrospect Model File

---
+++ ReGo/Sources/Models/Retrospect.swift
@@ -0,0 +1,34 @@
+//
+//  Retrospect.swift
+//  ReGo
+//
+//  Created by JuHyeon Seong on 5/12/25.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+
+enum RetrospectCategory: String, Codable {
+	case category1
+    case category2
+    case category3
+    case category4
+    case category5
+}
+
+@Model
+class Retrospect: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var content: String
+    var date: Date
+    var category: RetrospectCategory
+
+    init(title: String, content: String, date: Date, category: RetrospectCategory) {
+        self.title = title
+        self.content = content
+        self.date = date
+        self.category = category
+    }
+}
