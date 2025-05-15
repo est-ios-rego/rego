8c9124b6b1058f0a0679938e709a1cdf1b347fd9
성주현 <qpqoqiqu12@gmail.com>
2025-05-15 16:21:57 +0900

    Extensions DocC 작업

--- ReGo/Sources/Extensions/Calendar+Extensions.swift
+++ ReGo/Sources/Extensions/Calendar+Extensions.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 extension Calendar {
+
+    /// 해당 월 첫날이 무슨 요일인지 반환하는 함수
     func getFirstWeekDayOfMonth(in date: Date) -> Int {
         let components = Self.current.dateComponents([.year, .month], from: date)
         let firstDayOfMonth = Self.current.date(from: components)!
