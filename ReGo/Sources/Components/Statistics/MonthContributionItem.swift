304d759c9a0583d002098cb1142d126acc2d2ecf
성주현 <qpqoqiqu12@gmail.com>
2025-05-15 14:08:55 +0900

    통계 뷰 ContributionChartItem DocC 작업

--- ReGo/Sources/Components/Statistics/MonthContributionItem.swift
+++ ReGo/Sources/Components/Statistics/MonthContributionItem.swift
@@ -14,11 +14,18 @@ import SwiftData
 /// 해당 날짜의 회고 작성 수에 따라 배경색 투명도가 조절.
 /// 선택 시 하단에 원형 마커 표시.
 struct MonthContributionItem: View {
+
     /// 현재 앱의 색상 스킴 (라이트/다크 모드) 환경 변수.
     @Environment(\.colorScheme) var colorScheme
 
+    /// 기여도 수준에 따른 기본 색상. 다크/라이트 모드에 따라 다른 색상 사용.
+    var color: Color {
+        colorScheme == .light ? Color.regoPositive : Color.regoNegative
+    }
+
     /// 해당 날짜의 회고 작성 건수.
     let count: Int
+
     /// 해당 날짜의 일(day)
     let day: Int
 
@@ -31,11 +38,6 @@ struct MonthContributionItem: View {
     ///
     /// `day`와 일치하면 선택된 상태로 간주.
     @Binding var selectedDay: Int?
-
-    /// 기여도 수준에 따른 기본 색상. 다크/라이트 모드에 따라 다른 색상 사용.
-    var color: Color {
-        colorScheme == .light ? Color.regoPositive : Color.regoNegative
-    }
 
     /// 셀의 채우기 색상.
     ///
